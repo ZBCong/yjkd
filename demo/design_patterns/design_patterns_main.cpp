8e7b31e748132d8dfbc7ec625185f79512449652
siren186 <siren186@163.com>
2015-06-15 15:15:09 +0800

    + 添加factory method模式的示例

--- demo/design_patterns/design_patterns_main.cpp
+++ demo/design_patterns/design_patterns_main.cpp
@@ -1,8 +1,14 @@
 #include "stdafx.h"
 #include "singleton.hpp"
+#include "factory_method.hpp"
 
 TEST(DESIGN_PATTERNS, SINGLETON)
 {
     CLazyMan::Instance().DoSomething();
     y::Singleton<CHungryMan>::instance().DoSomething();
+}
+
+TEST(DESIGN_PATTERNS, FACTORY_METHOD)
+{
+    FactoryMethodClient::main();
 }
---
+++ demo/design_patterns/factory_method.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#ifndef interface
+#define interface struct
+#endif // interface
+
+// 工厂模式,对象创建型模式.使对象的创建工作延迟到子类
+
+interface ILightBase
+{
+    virtual void TurnOn()  = 0;
+    virtual void TurnOff() = 0;
+};
+
+class CBulbLight : public ILightBase
+{
+public:
+    virtual void TurnOn()  {}
+    virtual void TurnOff() {}
+};
+
+class CTubeLight : public ILightBase
+{
+public:
+    virtual void TurnOn()  {}
+    virtual void TurnOff() {}
+};
+
+interface ICreatorBase
+{
+    virtual ILightBase* Create() = 0;
+};
+
+class CBulbCreator : public ICreatorBase
+{
+public:
+    virtual ILightBase* Create() { return new CBulbLight(); }
+};
+
+class TubeCreator : public ICreatorBase
+{
+public:
+    virtual ILightBase* Create() { return new CTubeLight(); }
+};
+
+class FactoryMethodClient
+{
+public:
+    static void main()
+    {
+        ICreatorBase* pBulbCreator = new CBulbCreator();
+        ICreatorBase* pTubeCreator = new TubeCreator();
+
+        ILightBase* pBulbLight = pBulbCreator->Create();
+        ILightBase* pTubeLight = pTubeCreator->Create();
+
+        pBulbLight->TurnOn();
+        pBulbLight->TurnOff();
+
+        pTubeLight->TurnOn();
+        pTubeLight->TurnOff();
+
+        delete pBulbCreator;
+        delete pTubeCreator;
+        delete pBulbLight;
+        delete pTubeLight;
+    }
+};
