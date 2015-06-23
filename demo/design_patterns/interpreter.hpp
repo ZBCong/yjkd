413979baffd06b60e87bc8026b1221c2ebdc4544
siren186 <siren186@163.com>
2015-06-23 17:32:00 +0800

    * 得空整理下代码.重新写一个interpreter模式的例子

--- demo/design_patterns/interpreter.hpp
+++ demo/design_patterns/interpreter.hpp
@@ -1,159 +1,200 @@
 #pragma once
 #include "design_patterns_def.h"
+#include <map>
+#include <string>
+#include <stack>
 
 // 解释器模式
 // 1) AbstractExpression（抽象表达式）：声明一个抽象的解释操作，这个接口为抽象语法树中所有的节点所共享。
-// 2) TerminalExpression（终结符表达式）：实现与文法中的终结符相关联的解释操作。
+// 2) TerminalExpression（终结符表达式）：例如A◎B这个表达式中的A,B即是终止符。
 //    实现抽象表达式中所要求的接口，主要是一个interpreter()方法。文法中每一个终结符都有一个具体终结表达式与之相对应。
-// 3) NonterminalExpression（非终结符表达式）：为文法中的非终结符实现解释操作。
-// 4) Context：包含解释器之外的一些全局信息。
+// 3) NonterminalExpression（非终结符表达式）：上式中的◎符号即为非终止符。
+// 4) Context：需要被解释的东西
 // 缺点: 解释器模式为文法中的每一条规则至少定义了一个类，因此包含许多规则的文法可能难以管理和维护。
 //       建议当文法非常复杂时，使用其他的技术如语法分析程序或编译器生成器来处理。
+// 满足“业务规则频繁变化，且类似的模式不断重复出现，并且容易抽象为语法规则的问题”适合使用Interpreter模式。
 
 
-// Context:此处为演奏内容类
-class CPlayContext
+// 代码示例:输入一个模型公式（加减四则运算），然后输入模型中的参数，运算出结果。
+// 如输入公式: "a+b-c"
+// 再分别输入a,b,c的值,计算公式的结果
+
+namespace interpreter
+{
+
+// global definitions
+typedef char                              MapKeyType;
+typedef int                               MapValueType;
+typedef std::map<MapKeyType,MapValueType> MapType;
+
+
+interface IExpresstion
+{
+    virtual MapValueType Interpret(const MapType& m) = 0;
+    virtual ~IExpresstion() {}
+};
+
+class SymbolExprData
 {
 public:
-    void SetContext(const std::string& sContext) { m_sContext = sContext; }
-    std::string GetContext() const { return m_sContext; }
-    bool IsEmpty() const { return m_sContext.size() <= 0; }
+    SymbolExprData(IExpresstion* pLeftExpr, IExpresstion* pRightExpr)
+        : m_pLeftExpr(pLeftExpr)
+        , m_pRightExpr(pRightExpr)
+    {}
+
+    ~SymbolExprData()
+    {
+        if (m_pLeftExpr)  delete m_pLeftExpr;
+        if (m_pRightExpr) delete m_pRightExpr;
+    }
+
+    MapValueType InterpretLeft(const MapType& m)
+    {
+        if (m_pLeftExpr)
+        {
+            return m_pLeftExpr->Interpret(m);
+        }
+        return -1;
+    }
+
+    MapValueType InterpretRight(const MapType& m)
+    {
+        if (m_pRightExpr)
+        {
+            return m_pRightExpr->Interpret(m);
+        }
+        return -1;
+    }
 
 private:
-    std::string m_sContext;
+    IExpresstion* m_pLeftExpr;
+    IExpresstion* m_pRightExpr;
 };
 
-// AbstractExpression: 此处为表达式接口
-interface IExpression
+class CValExpr : public IExpresstion
 {
-    void Interpret(CPlayContext* context)
+public:
+    CValExpr(const MapKeyType& key) : m_key(key)
+    {}
+
+    virtual MapValueType Interpret(const MapType& m)
     {
-        std::string s1 = context->GetContext();
-        std::string buf;
-        std::string s2;
-
-        if(context->GetContext().length()==0)
+        MapType::const_iterator it = m.find(m_key);
+        if (it != m.end())
         {
-            return;
+            return it->second;
         }
-        else
+        return MapValueType(0);
+    }
+
+private:
+    MapKeyType m_key;
+};
+
+class CAddExpr : public IExpresstion
+{
+public:
+    CAddExpr(IExpresstion* pLeftExpr, IExpresstion* pRightExpr) : m_data(pLeftExpr, pRightExpr)
+    {}
+
+    virtual MapValueType Interpret( const MapType& m )
+    {
+        return m_data.InterpretLeft(m) + m_data.InterpretRight(m);
+    }
+
+private:
+    SymbolExprData m_data;
+};
+
+class CSubExpr : public IExpresstion
+{
+public:
+    CSubExpr(IExpresstion* pLeftExpr, IExpresstion* pRightExpr) : m_data(pLeftExpr, pRightExpr)
+    {}
+
+    virtual MapValueType Interpret( const MapType& m )
+    {
+        return m_data.InterpretLeft(m) - m_data.InterpretRight(m);
+    }
+
+private:
+    SymbolExprData m_data;
+};
+
+class Calculator
+{
+public:
+    Calculator(const std::string& sExpr)
+    {
+        _Analysis(sExpr);
+    }
+
+    ~Calculator()
+    {
+        if (m_expr)
         {
-            std::vector<std::string> vec;
+            delete m_expr;
+        }
+    }
 
-            std::stringstream ss(context->GetContext());
+    MapValueType Do(const MapType& m)
+    {
+        if (m_expr)
+        {
+            return m_expr->Interpret(m);
+        }
+        return -1;
+    }
 
-            while(ss >> buf)
-                vec.push_back(buf);
+private:
+    void _Analysis(const std::string& sExpr)
+    {
+        std::stack<IExpresstion*> stackExpr;
+        IExpresstion* pLeftExpr  = NULL;
+        IExpresstion* pRightExpr = NULL;
 
-            std::string playKey=vec[0];
-            std::string playValue=vec[1];
-            Excute(playKey,playValue);
-
-            vec.erase(vec.begin(),vec.begin()+2);
-            std::vector<std::string>::iterator it;
-            for(it=vec.begin();it!=vec.end();it++)
+        for (std::string::const_iterator it = sExpr.begin(); it != sExpr.end(); ++it)
+        {
+            switch (*it)
             {
-                s2+=*it;
-                if(it!=vec.end()-1)
-                    s2+=" ";
+            case '+':
+                pLeftExpr = stackExpr.top();
+                pRightExpr = new CValExpr(*(++it));
+                stackExpr.push(new CAddExpr(pLeftExpr, pRightExpr));
+                break;
+            case '-':
+                pLeftExpr = stackExpr.top();
+                pRightExpr = new CValExpr(*(++it));
+                stackExpr.push(new CSubExpr(pLeftExpr, pRightExpr));
+                break;
+            default:
+                stackExpr.push(new CValExpr(*it));
+                break;
             }
-
-            context->SetContext(s2);
         }
+        m_expr = stackExpr.top();
     }
-    virtual void Excute(std::string key,std::string value) = 0;
+
+private:
+    IExpresstion* m_expr;
 };
 
-//ConcreteExpression，此处为音符类
-class Note : public IExpression
-{
-public:
-    void Excute(std::string key,std::string value)
-    {
-        std::string note;
-        switch(key[0])
-        {
-        case 'C':
-            note="1";
-            break;
-        case 'D':
-            note="2";
-            break;
-        case 'E':
-            note="3";
-            break;
-        case 'F':
-            note="4";
-            break;
-        case 'G':
-            note="5";
-            break;
-        case 'A':
-            note="6";
-            break;
-        case 'B':
-            note="7";
-            break;
-        }
-        std::cout<<note<<" ";
-    }
-};
-
-//ConcreteExpression，此处为音阶类
-class Scale : public IExpression
-{
-public:
-    void Excute(std::string key, std::string value)
-    {
-        std::string scale;
-        switch(value[0])
-        {
-        case '1':
-            scale="低音";
-            break;
-        case '2':
-            scale="中音";
-            break;
-        case'3':
-            scale="高音";
-            break;
-        }
-        std::cout<<scale<<" ";
-    }
-};
+} // end of namespace interpreter
 
 class CInterpreterClient
 {
 public:
     static void main()
     {
-        CPlayContext context;
-        context.SetContext("O 2 E 0.5 G 0.5 A 3 E 0.5 G 0.5 D 3 E 0.5 G 0.5 A 0.5 O 3 C 1 O 2 A 0.5 G 1 C 0.5 E 0.5 D 3");
+        using namespace interpreter;
 
-        IExpression* pExpression = NULL;
+        MapType m;
+        m['a'] = 10;
+        m['b'] = 20;
+        m['c'] = 5;
 
-        while(!context.IsEmpty())
-        {
-            char c = context.GetContext()[0];
-            switch(c)
-            {
-            case 'O':
-                pExpression=new Scale();
-                break;
-            case 'C':
-            case 'D':
-            case 'E':
-            case 'F':
-            case 'G':
-            case 'A':
-            case 'B':
-            case 'P':
-                pExpression=new Note();
-                break;
-            }
-
-            pExpression->Interpret(&context);
-            delete pExpression;
-        }
+        Calculator c("a+b-c");
+        int n = c.Do(m);
+        ASSERT_EQ(n, 25);
     }
 };
