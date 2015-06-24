ece0f004f54c2bb412b00ab49f02a9c534714c33
siren186 <siren186@163.com>
2015-06-24 17:27:23 +0800

    * 完成单一继承的情况

--- demo/polymorphism/polymorphism_main.cpp
+++ demo/polymorphism/polymorphism_main.cpp
@@ -2,7 +2,7 @@
 #pragma warning(disable:4312)
 #include "empty_class.h"
 #include "virtual_base_class.h"
-#include "child_class.h"
+#include "single_inheritance_child_class.h"
 
 /**
  * 浅析下vs2005下32位程序的多态实现原理
@@ -15,4 +15,4 @@ TEST(POLYMORPHISM, BASE_VIRTUAL_CLASS)
 { CVirtualBaseClassClient::main(); }
 
 TEST(POLYMORPHISM, CHILD_VIRTUAL_CLASS)
-{ CChildClassClient::main(); }
+{ CSingleInheritanceChildClass::main(); }
--- demo/polymorphism/single_inheritance_child_class.h
+++ demo/polymorphism/single_inheritance_child_class.h
@@ -14,10 +14,31 @@ private:
     int m_c2;
 };
 
-class CChildClassClient
+class CChildWithOverride : public CBase
+{
+public:
+    CChildWithOverride() : m_c1(1000), m_c2(2000) {}
+
+    virtual int c1() { return 111; }
+    virtual int f2() { return 666; } // 覆盖了父类的f2方法
+    virtual int c2() { return 222; }
+
+private:
+    int m_c1;
+    int m_c2;
+};
+
+class CSingleInheritanceChildClass
 {
 public:
     static void main()
+    {
+        _test1();
+        _test2();
+    }
+
+private:
+    static void _test1()
     {
         CChildWithoutOverride c;
         int* pAddrOfC = (int*)(&c); // 取对象的首地址
@@ -50,5 +71,38 @@ public:
         ASSERT_EQ(222, pChildC2());                                 // 调用第一个虚函数CBase::f3()
         PFun pChildC3 = (PFun)*(pAddrOfFirstVitualFuncInBase+5);   // 虚函数表首地址加2,得到下下一个虚函数地址
         ASSERT_EQ(333, pChildC3());                                 // 调用第一个虚函数CBase::f3()
+    }
+
+    static void _test2()
+    {
+        CChildWithOverride c;
+        int* pAddrOfC = (int*)(&c); // 取对象的首地址
+
+        int* pAddrOfFirstVitualFuncInBase = (int*)(*pAddrOfC);  // 对象首地址    -> 虚函数表
+        int* pBaseMember1  = (int*)(pAddrOfC + 1);              // 对象首地址加1 -> 父类成员1
+        int* pBaseMember2  = (int*)(pAddrOfC + 2);              // 对象首地址加2 -> 父类成员2
+        int* pChildMember1 = (int*)(pAddrOfC + 3);              // 对象首地址加1 -> 自身成员1
+        int* pChildMember2 = (int*)(pAddrOfC + 4);              // 对象首地址加2 -> 自身成员2
+
+        ASSERT_EQ(100, *pBaseMember1);
+        ASSERT_EQ(200, *pBaseMember2);
+        ASSERT_EQ(1000, *pChildMember1);
+        ASSERT_EQ(2000, *pChildMember2);
+
+        typedef int (*PFun)();
+
+        // 调用基类的函数
+        PFun pBaseF1 = (PFun)(*pAddrOfFirstVitualFuncInBase);     // 取虚函数表的首地址所指内容,取第一个虚函数地址
+        ASSERT_EQ(10, pBaseF1());                                 // 调用第一个虚函数CBase::f1()
+        PFun pBaseF2 = (PFun)*(pAddrOfFirstVitualFuncInBase + 1); // 虚函数表首地址加1,得到下一个虚函数地址
+        ASSERT_EQ(666, pBaseF2());                                // 调用第一个虚函数CChildWithOverride::f2()
+        PFun pBaseF3 = (PFun)*(pAddrOfFirstVitualFuncInBase + 2); // 虚函数表首地址加2,得到下下一个虚函数地址
+        ASSERT_EQ(30, pBaseF3());                                 // 调用第一个虚函数CBase::f3()
+
+        // 调用子类自身的虚函数
+        PFun pChildC1 = (PFun)*(pAddrOfFirstVitualFuncInBase+3);   // 虚函数表首地址加2,得到下下一个虚函数地址
+        ASSERT_EQ(111, pChildC1());                                // 调用第一个虚函数CBase::f3()
+        PFun pChildC2 = (PFun)*(pAddrOfFirstVitualFuncInBase+4);   // 虚函数表首地址加2,得到下下一个虚函数地址
+        ASSERT_EQ(222, pChildC2());                                // 调用第一个虚函数CBase::f3()
     }
 };
